9cbf8c597ead2fb7e25ae4849a06cc0dc3ffa94c
abiliobitt <abilio.bjr@gmail.com>
2023-06-18 20:46:41 -0300

    finalizando código para funcionar, falta melhorar o código

--- API/main.py
+++ API/main.py
@@ -1,5 +1,7 @@
 from fastapi import BackgroundTasks, FastAPI
 import inject
+import os
+import json
 
 from services.data_service import DataService
 from settings import adapter_binders
@@ -7,7 +9,7 @@ from settings import adapter_binders
 
 app = FastAPI()
 
-@app.get('/fetchDataToFiles')
+@app.get('/fetch_data_to_file')
 async def read_root(background_tasks: BackgroundTasks):
     with open('DataServiceIsRunning.txt', 'r') as file:
         data = file.read().rstrip()
@@ -18,6 +20,29 @@ async def read_root(background_tasks: BackgroundTasks):
         background_tasks.add_task(data_service.get_data)
         return {'isRunning': 'false'}
 
+@app.get('/generate_data')
+async def read_root():
+    data_service: DataService = inject.instance(DataService)
+    await data_service.generateStatesData()
+    return {'status': 'done'}
+
+@app.get('/potency_by_state_and_time_range')
+async def read_root():
+    if (os.path.exists('potency_by_state_and_time_range.json')):
+        with open('potency_by_state_and_time_range.json') as f:
+            data = json.load(f)
+        return data
+    else:
+        return {}
+
+@app.get('/potency_by_uf_and_class')
+async def read_root():
+    if (os.path.exists('potency_by_uf_and_class.json')):
+        with open('potency_by_uf_and_class.json') as f:
+            data = json.load(f)
+        return data
+    else:
+        return {}
 
 def configure(binder):
     for config in adapter_binders:
--- API/services/data_service.py
+++ API/services/data_service.py
@@ -2,11 +2,12 @@ import inject
 import json
 import os
 import pandas as pd
+import random
 
 from infra.http_client.httpx.http_client import HttpClient
 from settings.data.dataSettings import DataSettings
 
-def write_json(new_data, filename='data.json'):
+def write_json(new_data, filename):
     if os.path.isfile(filename):
         with open(filename, 'r+') as file:
             file_data = json.load(file)
@@ -22,27 +23,67 @@ def write_json(new_data, filename='data.json'):
 class DataService:
     @staticmethod
     async def get_data():
-        http_adapter: HttpClient = inject.instance(HttpClient)
-        offset = 0
-        f = open("DataServiceIsRunning.txt", "a")
-        f.truncate(0)
-        f.write("true")
-        f.close()
-        while offset < 4000:
-            response = await http_adapter.get(
-                f"{DataSettings.URL}&limit=2&offset={0 if offset == 0 else offset}",
-                headers={
-                }
-            )
-            offset = offset + 2000
-            for i in response.payload['result']['records']:
-                write_json(i)
-        f = open("DataServiceIsRunning.txt", "a")
+        try:
+            http_adapter: HttpClient = inject.instance(HttpClient)
+            offset = 24407
+            f = open('DataServiceIsRunning.txt', 'a')
+            f.truncate(0)
+            f.write('true')
+            f.close()
+            while offset < 60000:
+                response = await http_adapter.get(
+                    f'{DataSettings.URL}&limit=2000&offset={0 if offset == 0 else offset}',
+                    headers={
+                    }
+                )
+                offset = offset + 2000
 
-        json_string = open('data.json')
-        read = pd.read_json(json_string)
-        read.to_csv('data.csv')
+                # json_file_data_frame = pd.read_json('records_data.json')
+                # response_data_frame = pd.json_normalize(response.payload['result']['records'])
+                # new_data_frame = pd.concat([json_file_data_frame, pd.DataFrame([response_data_frame])], ignore_index=True)
+                # print(new_data_frame)
 
-        f.truncate(0)
-        f.write("false")
-        f.close()
+
+                for i in response.payload['result']['records']:
+                    write_json(i, 'records_data.json')
+            
+            f = open('DataServiceIsRunning.txt', 'a')
+            f.truncate(0)
+            f.write('false')
+            f.close()
+        except NameError:
+            print(NameError)
+            f = open('DataServiceIsRunning.txt', 'a')
+            f.truncate(0)
+            f.write('false')
+            f.close()
+
+    @staticmethod
+    async def generateStatesData():
+        try:
+             with open('records_data.json','r') as f:
+                data = json.loads(f.read())
+                df_nested_list = pd.json_normalize(data, record_path =['records'])
+
+                #transformando datas do dataframe para conseguir gerar os gráficos
+                ufs_df = df_nested_list.sort_values(['SigUF'])
+                ufs_df = ufs_df.reset_index()
+                random_dates = ['06/2023', '07/2023', '08/2023', '09/2023', '10/2023']
+                for row in ufs_df.itertuples(index=True):
+                    ufs_df.loc[row.Index, 'AnmPeriodoReferencia'] = random.choice(random_dates)
+                
+                # transformando strings em float
+                ufs_df['MdaPotenciaInstaladaKW'] = ufs_df['MdaPotenciaInstaladaKW'].str.replace(',','.').astype('float')
+
+                # Potência instalada por estado e classe de empreendimento
+                count_potency_by_state_and_class = ufs_df.groupby(['SigUF','DscClasseConsumo', 'AnmPeriodoReferencia'], as_index=False)['MdaPotenciaInstaladaKW'].count()
+                count_potency_by_state_and_class.to_csv('potency_by_uf_and_class.csv')
+                count_potency_by_state_and_class.to_json('potency_by_uf_and_class.json')
+
+                # Potência instalada por estado
+                count_potency_by_state_and_time_range = ufs_df.groupby(['SigUF', 'AnmPeriodoReferencia'], as_index=False)['MdaPotenciaInstaladaKW'].count()
+                count_potency_by_state_and_time_range.to_csv('potency_by_state_and_time_range.csv')
+                count_potency_by_state_and_time_range.to_json('potency_by_state_and_time_range.json')
+
+        except NameError:
+            print(NameError)
